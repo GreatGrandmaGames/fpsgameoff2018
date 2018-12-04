9a5946eef33a7588467f36427fdb5d27e33a7dff
Carlos Michael Rodriguez <cmr624@nyu.edu>
2018-12-04 09:22:01 -0500

    added some prefabs

---
+++ Assets/GithubGameJam2018/GameManager/Inventory/GunWheel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunWheel : MonoBehaviour {
+
+    private Transform[] gameObjects;
+	// Use this for initialization
+	void Start () {
+        gameObjects = GetComponentsInChildren<Transform>(true);
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            Debug.Log(gameObjects[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            gameObjects[2].gameObject.SetActive(true);
+            gameObjects[1].gameObject.SetActive(false);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            gameObjects[1].gameObject.SetActive(true);
+            gameObjects[2].gameObject.SetActive(false);
+        }
+    }
+}
